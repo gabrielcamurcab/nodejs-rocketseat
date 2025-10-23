ed284779a6607a3480b089a6a6b97a8d645d7225
Gabriel Camurça <gabriel@brtech.dev>
2025-10-23 16:33:40 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 🧠 Estudos de Node.js — Rocketseat Ignite
+
+Repositório criado para registrar e praticar os estudos realizados durante a formação **Node.js** da [Rocketseat](https://www.rocketseat.com.br/), dentro da assinatura **One**.
+
+---
+
+## 🚀 Objetivo
+
+Consolidar fundamentos e boas práticas do desenvolvimento backend com **Node.js**, aplicando **Clean Architecture**, **SOLID**, **DDD** e **Testes Automatizados**, conforme ensinado na trilha **Ignite** da Rocketseat.
+
+> “A prática leva à excelência — e o código limpo leva à paz de espírito.” ✨
+
+---
---
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -0,0 +1,26 @@
+import http from 'node:http';
+
+const users = [];
+
+const server = http.createServer((req, res) => {
+    const { method, url } = req;
+
+    if (method === 'GET' && url === '/users') {
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users));
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'Zezin',
+            email: 'zezin@gmail.com'
+        });
+        return res.end('Criação de usuário')
+    }
+
+    return res.end('Hello Gabriel');
+});
+
+server.listen(3333);
